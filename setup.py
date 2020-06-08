a9cd80f7387d6b72120fc3155791ae140eaaadd2
xuzhaoxiong <17610739793@163.com>
2020-06-08 11:19:48 +0800

    "acl plugin 0.1.1"

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@
 from setuptools import setup, find_packages
 
 description = "Bottle plugin acl, api access control"
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(
-    name='bottle-aclPlugin',  # 上传的包名
-    version="0.1.0",  # 上传的版本
-    packages=find_packages(),  # 导入目录下所有__init__.py包
-    install_requires=REQUIREMENTS,  # 需要引入的第三方库
-    description=description,  # 一段描述的话
+    name='bottle-aclPlugin',
+    version="0.1.1",
+    packages=find_packages(),
+    description=description,
     author="Xu_beixiong",
     author_email='17610739793@163.com',
     license='MIT',
@@ -23,7 +21,6 @@ setup(
         'Framework :: Bottle',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.5',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
